bed082bd27a188c7b3d98402872ba99f99d16734
Wyatt Murray <wyattmurray1235@gmail.com>
2023-10-19 15:27:27 -0400

    test bench planning

---
+++ root/VR Testing/Assets/InstantiationTestBench.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiationTestBench : MonoBehaviour
+{
+    public GameObject[] prefabsToInstantiate; //done in the inspector
+
+    public void SpawnMag()
+    {
+
+    }
+
+    public void SpawnSword()
+    {
+
+    }
+
+    public void SpawnRifle()
+    {
+
+    }
+
+}
+
